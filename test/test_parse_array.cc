c11c7e8391ea4e17b9b43af36515c2987013d07e
N.O.Atomusk <orzkey@outlook.com>
2022-08-28 01:59:58 +0800

    :white_check_mark:add test case of parse array error

--- test/test_parse_array.cc
+++ test/test_parse_array.cc
@@ -1,1 +1,16 @@
 #include "test_utils.h"
+
+TEST(PARSE_ARRAY, ARRAY)
+{
+    // auto v = tijson::parse("[ ]");
+    // v      = tijson::parse("[ null , false , true , 123 , \"abc\" ]");
+    // v      = tijson::parse("[ [ ] , [ 0 ] , [ 0 , 1 ] , [ 0 , 1 , 2 ] ]");
+}
+
+TEST(PARSE_ARRAY, MISS_COMMA_OR_SQUARE_BRACKET)
+{
+    EXPECT_PARSE_THROW_MESSAGE("[1", "MISS_COMMA_OR_SQUARE_BRACKET");
+    EXPECT_PARSE_THROW_MESSAGE("[1}", "MISS_COMMA_OR_SQUARE_BRACKET");
+    EXPECT_PARSE_THROW_MESSAGE("[1 2", "MISS_COMMA_OR_SQUARE_BRACKET");
+    EXPECT_PARSE_THROW_MESSAGE("[[]", "MISS_COMMA_OR_SQUARE_BRACKET");
+}
